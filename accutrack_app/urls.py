f6108661e786a6fd2bbd4b536937ecca3eaa7c6e
ankuxsshh <ankuxshh72@gmail.com>
2024-12-04 17:00:29 +0530

    first commit

--- accutrack_app/urls.py
+++ accutrack_app/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), 
+    path('about', views.about, name='about'), 
+    path('services', views.services, name='services'), 
+    path('gallery', views.gallery, name='gallery'), 
+    path('contact', views.contact, name='contact'), 
 ]
--- accutrack_app/views.py
+++ accutrack_app/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def contact(request):
+    return render(request, 'contact.html')
