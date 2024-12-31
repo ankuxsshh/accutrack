13d14b8c8897e4e1cea44fe64fb5a516f4bd83ab
ankuxsshh <ankuxshh72@gmail.com>
2024-12-31 16:16:19 +0530

    first commit

---
+++ accutrack_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def contact(request):
+    return render(request, 'contact.html')
